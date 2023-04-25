ebc30e924f4cd679724f9f0a95f9a2aab08371ba
XvcreAm <47131367+XvcreAm@users.noreply.github.com>
2023-04-25 14:20:23 +0700

    Final UserController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 
 const bcrypt = require('../helpers/bcrypt');
 const jwt = require('../helpers/jwt');
-const { pool } = require('../config/index');
+const pool  = require('../config');
+
 
 
 const userController = {};
@@ -9,15 +10,16 @@ const userController = {};
 
 userController.register = async (req, res) => {
   try {
-    
     const { email, password } = req.body;
-    const result = await pool.query('SELECT * FROM user WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM "user" WHERE email = $1', [email]);
     const user = result.rows[0];
     if (user) {
       return res.status(400).json({ message: 'Email already taken' });
     }
-    await pool.query('INSERT INTO user (email, password) VALUES ($1, $2)', [email, hashedPassword]);
+    const quer = await pool.query('INSERT INTO "user" (email, password) VALUES ($1, $2)', [email, bcrypt.hashPassword(password)]);
     
+    res.status(200).json({message: 'User Berhasil Ditambah'})
+  
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
@@ -28,15 +30,19 @@ userController.register = async (req, res) => {
 userController.login = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const result = await pool.query('SELECT * FROM user WHERE email = $1', [email]);
+    const result = await pool.query('SELECT * FROM "user" WHERE email = $1', [email]);
     const user = result.rows[0];
     if (!user) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
-    const match = await bcrypt.compare(password, user.password);
+    const match = await bcrypt.comparePassword(password, user.password);
     if (!match) {
       return res.status(401).json({ message: 'Invalid credentials' });
     }
+    
+    const token = jwt.generateToken(user)
+
+    res.status(200).json({token: token})
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const user = require('../controllers/userController')
-const reflection = require('../controllers/reflectionController')
+// const reflection = require('../controllers/reflectionController')
 const auth = require('../middlewares/auth')
 
 router.post("/api/v1/users/register",user.register)
 router.post("/api/v1/users/login",user.login)
-router.post('/api/v1/reflections',auth,reflection.createReflection)
-router.get('/api/v1/reflections',auth,reflection.getReflection)
-router.delete('/api/v1/reflections/:id',auth,reflection.deleteReflection)
-router.put('/api/v1/reflections/:id',auth,reflection.updateReflection)
+// router.post('/api/v1/reflections',auth,reflection.createReflection)
+// router.get('/api/v1/reflections',auth,reflection.getReflection)
+// router.delete('/api/v1/reflections/:id',auth,reflection.deleteReflection)
+// router.put('/api/v1/reflections/:id',auth,reflection.updateReflection)
 
 module.exports = router;
